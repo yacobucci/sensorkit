07bb63a9a54688ddd753f2b937b997292f90785b
Matthew Yacobucci <yacobucci@gmail.com>
2024-12-14 11:38:26 -0700

    Reorganizing devices into constants and profiles

--- sensorkit/__init__.py
+++ sensorkit/__init__.py
@@ -1,8 +1,18 @@
-__all__ = [ 'config', 'controls', 'datastructures', 'devices', 'devicetree', 'meters' ]
+__all__ = [ 'config',
+           'constants',
+           'controls',
+           'datastructures',
+           'devices',
+           'devicetree',
+           'meters',
+           'profiles'
+]
 
 from . import config
+from . import constants
 from . import controls
 from . import datastructures
 from . import devices
 from . import devicetree
 from . import meters
+from . import profiles
